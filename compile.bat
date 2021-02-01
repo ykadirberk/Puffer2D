c450f91962174ad2c2f23f3d6e0a97df97bd5604
ykadirberk <ykadirberk2@gmail.com>
2021-02-02 01:57:00 +0300

    added static linking for gcc libraries

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 @echo off
 g++ -c ./src/*.cpp -I"C:/SFML/Install/Include" -std=c++11 -Os
-@echo [COMPILATION] done.
-g++ *.o -g -o ./release/app -L"C:/SFML/Install/lib" -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system -Os
-@echo [CREATION] app.exe created.
-del /f *.o
+echo [COMPILATION] done.
+g++ -static -static-libgcc -static-libstdc++ *.o -g -o ./release/app -L"C:/SFML/Install/lib" -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lsfml-system
+echo [CREATION] app.exe created.
+del *.o
 PAUSE
